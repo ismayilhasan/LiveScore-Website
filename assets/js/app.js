10c4d3b23141bdd57f6f98b730d049187e76bcc8
unknown <ismayilrha@code.edu.az>
2022-10-09 15:06:37 +0400

    added filter engine for leauges

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,12 +5,35 @@ const closeIcon = document.querySelector('#closeIcon')
 
 
 // Events
-menuIcon.addEventListener('click', () => {
+menuIcon.addEventListener('click', (event) => {
     sidebar.classList.add('active-sidebar')
 })
 
-closeIcon.addEventListener('click', () => {
+closeIcon.addEventListener('click', (event) => {
     sidebar.classList.remove('active-sidebar')
 
 })
+
+// Filter Search Engine For Leauges
+document.querySelector('#searchInput').addEventListener('input',filterList)
+function filterList()
+{
+    const searchInput = document.querySelector('#searchInput')
+    const filter = searchInput.value.toLowerCase();
+    
+    const listItems = document.querySelectorAll('.list-group-item')
+
+    listItems.forEach((item) => {
+        let text = item.textContent
+
+        if(text.toLowerCase().includes(filter.toLowerCase()))
+        {
+            item.style.display = '';
+        }
+        else{
+            item.style.display = 'none';
+        }
+    })
+    
+}
 

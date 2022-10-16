682d46d742e507fcd4004b665d0e1a7bd5b4899e
unknown <ismayilrha@code.edu.az>
2022-10-16 17:42:42 +0400

    fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,24 +17,7 @@ for (const menuIcon of menuIcons ) {
 // Events
 
 
-window.onscroll = function()
-{
-  if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100)
-  {
-    let scrool_btn = document.querySelector('#scrollBtn');
-    scrool_btn.style.opacity = '1'; 
-    
-  }
-  else{
-    let scrool_btn = document.querySelector('#scrollBtn');
-    scrool_btn.style.opacity = '0';
-  }
-}
 
-scrollBtn.addEventListener('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0});
-});
 
 // Slider Settings
 $('.categories').slick({
--- assets/js/filter.js
+++ assets/js/filter.js
@@ -20,3 +20,22 @@ function filterList()
     })
     
 }
+
+window.onscroll = function()
+{
+  if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100)
+  {
+    let scrool_btn = document.querySelector('#scrollBtn');
+    scrool_btn.style.opacity = '1'; 
+    
+  }
+  else{
+    let scrool_btn = document.querySelector('#scrollBtn');
+    scrool_btn.style.opacity = '0';
+  }
+}
+
+scrollBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0});
+});

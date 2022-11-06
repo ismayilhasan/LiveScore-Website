2402f9d767d0662b74bed88bb7dc1da710762f0d
unknown <ismayilrha@code.edu.az>
2022-11-06 16:24:40 +0400

    adding rankings details privacy-notes and contact pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,19 +6,59 @@ let scrollBtn = document.querySelector('#scrollBtn')
 const calendarIcon = document.querySelector('.calendar-img')
 const tabButtons = document.querySelectorAll('.tabs')
 const tabsContentDivs = document.querySelectorAll('.current-content')
+const matches = document.querySelectorAll('#match')
+const currentMatchContent = document.getElementById('currentMatchContent')
+const closeIconCurrentMatchContent = document.getElementById('closeIconCurrentMatchContent')
+const rankingsTabs = document.querySelectorAll('.tabs-rankings')
+const rankingTables = document.querySelectorAll('.table-content')
+const viewTypeBtns = document.querySelectorAll(".view-type");
+const ViewTabs = document.querySelectorAll('.view-type')
+const playerViews = document.querySelectorAll('.player-views')
 
 
+// Functions
+function clearAllFilledTab() {
+  viewTypeBtns.forEach(viewTypeBtn => {
+      viewTypeBtn.querySelector(".icon").src = "/assets/photos/type.svg";
+  })
+}
 
+function changeTab(event) {
+  clearAllFilledTab();
+
+  const parent = event.target.parentElement;
+  const icon = parent.querySelector(".icon");
+ 
+
+  icon.src = icon.src.includes("type.svg")
+      ? icon.src = "/assets/photos/fillet-type.svg"
+      : "/assets/photos/type.svg";
+
+      let index = event.target.parentElement.getAttribute('data-id')
+    
+    for (const views of playerViews) {
+      if(views.getAttribute('data-id') === index)
+      {
+        views.classList.remove('d-none')
+      }
+      else{
+        views.classList.add('d-none')
+      }
+    }
+}
+
+
+// Events
+
+viewTypeBtns.forEach(viewTypeBtn => viewTypeBtn.addEventListener("click", changeTab));
 
 for (const tabs of tabButtons) {
   tabs.addEventListener('click',(event) => {
       let activeTab = document.querySelector('.active-tab-menu')
-
       activeTab.classList.remove('active-tab-menu')
       event.target.classList.add('active-tab-menu')    
 
       let index = event.target.getAttribute('id')
-    console.log(typeof (index));
       for (let div of tabsContentDivs) {
         if(div.getAttribute('id') === index)
           {
@@ -33,6 +73,26 @@ for (const tabs of tabButtons) {
   })
 }
 
+for (const ranking of rankingsTabs) {
+  ranking.addEventListener('click',(event) => {
+  
+    let index = event.target.parentElement.parentElement.getAttribute('id')
+    
+    
+    for (let table of rankingTables) {
+      if(table.getAttribute('id') === index)
+      {
+        table.classList.remove('d-none')
+      }
+      else{
+        table.classList.add('d-none')
+      }
+    }
+  })
+}
+
+
+
 
 for (const menuIcon of menuIcons ) {
     menuIcon.addEventListener('click', (event) => {
@@ -46,9 +106,21 @@ for (const menuIcon of menuIcons ) {
     
 }
 
+for (const match of matches) {
+
+  match.addEventListener('click', () => {
+    currentMatchContent.classList.add('displaying-match')
+  })
+}
+
+if(document.getElementById('closeIconCurrentMatchContent'))
+{
+  closeIconCurrentMatchContent .addEventListener('click',() => {
+    currentMatchContent.classList.remove('displaying-match')
+  })
+}
 
 
-// Events
 
 calendarIcon.addEventListener('click',() => {
   const calendar = document.querySelector('.calendar');
@@ -56,5 +128,8 @@ calendarIcon.addEventListener('click',() => {
 
 
 })
+
+
+
 
 
